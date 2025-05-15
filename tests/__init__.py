7134dc53631271802dbec22c34f3648a86869c46
Josh Vote <jjvote@gmail.com>
2025-05-15 14:31:08 +1000

    Fixing mypy

---
+++ tests/__init__.py
@@ -0,0 +1,25 @@
+from .test_procedures import (
+    Action,
+    Event,
+    Preconditions,
+    Step,
+    TestProcedure,
+    TestProcedureConfig,
+    TestProcedureDefinitionError,
+    TestProcedureId,
+    TestProcedures,
+)
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "TestProcedureId",
+    "TestProcedureDefinitionError",
+    "Event",
+    "Action",
+    "Step",
+    "Preconditions",
+    "TestProcedure",
+    "TestProcedures",
+    "TestProcedureConfig",
+]
