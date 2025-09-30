f6649008b68b90021c403134198a0906e344c845
Josh Vote <jjvote@gmail.com>
2025-10-01 08:32:46 +1000

    Tagging 1.0.0

--- cactus_test_definitions/__init__.py
+++ cactus_test_definitions/__init__.py
@@ -18,7 +18,7 @@ from cactus_test_definitions.variable_expressions import (
     try_extract_variable_expression,
 )
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 __all__ = [
     "TestProcedureDefinitionError",
